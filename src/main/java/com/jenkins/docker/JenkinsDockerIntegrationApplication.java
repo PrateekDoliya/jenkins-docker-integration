845af145c96008b4f201f947b7ac218b91c713eb
PrateekDoliya <prateekdoliya00@gmail.com>
2023-04-19 12:21:55 +0530

    Added logger to main class

--- src/main/java/com/jenkins/docker/JenkinsDockerIntegrationApplication.java
+++ src/main/java/com/jenkins/docker/JenkinsDockerIntegrationApplication.java
@@ -1,12 +1,19 @@
 package com.jenkins.docker;
 
+import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JenkinsDockerIntegrationApplication {
 
+	private static Logger logger  = LoggerFactory.getLogger(JenkinsDockerIntegrationApplication.class);
+	
 	public static void main(String[] args) {
+		
+		logger.info("This is logger ");
 		SpringApplication.run(JenkinsDockerIntegrationApplication.class, args);
 	}
 
